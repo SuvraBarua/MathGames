01b0732916c557c8d67febd9b893b09a0bcbd7b4
Suvra Barua <suvrabarua4@gmail.com>
2024-09-18 12:47:00 +0600

    Update TrueOrFalse.java

--- TrueOrFalse.java
+++ TrueOrFalse.java
@@ -1,13 +1,26 @@
 import java.util.*;
 
 class TrueOrFalse{
-  int numbers[4];
+  int numbers[];
 	void easy(){
   	Random rand = new Random();
-  	for(i : numbers){
-    	numbers[i] = rand.nextInt(20)+1;
+  	numbers = new int[4];
+  	numbers[0] = rand.nextInt(20)+1;
+  	numbers[1] = rand.nextInt(20)+1;
+  	numbers[2] = rand.nextInt(20)+1;
+  	numbers[3] = rand.nextInt(20)+1;
 	}
-  int[] trueFalse(){
+	
+	void medium(){
+  	Random rand = new Random();
+  	numbers = new int[4];
+  	numbers[0] = rand.nextInt(100)+1;
+  	numbers[1] = rand.nextInt(100)+1;
+  	numbers[2] = rand.nextInt(100)+1;
+  	numbers[3] = rand.nextInt(100)+1;
+	}
+	int[] trueFalse(){
+    easy();
     return numbers;
   }
 }
