57e048f136d7bc191c713a0607ef12b95da0ffa9
Suvra Barua <suvrabarua4@gmail.com>
2024-09-18 12:24:10 +0600

    Create TrueOrFalse.java

---
+++ TrueOrFalse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class TrueOrFalse{
+  int numbers[4];
+
+	void easy(){
+  	Random rand = new Random();
+  	for(i : numbers){
+    	numbers[i] = rand.nextInt();
+	}
+  int[] trueFalse(){
+    return numbers;
+  }
+}
